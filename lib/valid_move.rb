3edef2682ef786d2d95ebcc54e35e9e25e20d0da
Richard Dinwiddie <rdinwiddie3@gmail.com>
2018-09-19 17:56:46 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,7 @@ def  position_taken?(board,index)
   #return false if [" ", "", nil].include?(board[position])
   #return true if ["X", "O"].include?(board[position])
   #end
-  if array[index] == " " || aray[index] == "" || array[index] == nil
+  if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   else
     return true
