353630e8c092676ea45d644cc5d52b24f0e78ba3
Richard Dinwiddie <rdinwiddie3@gmail.com>
2018-09-19 17:52:55 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,3 +9,17 @@ def  position_taken?(board,position)
   return false if [" ", "", nil].include?(board[position])
   return true if ["X", "O"].include?(board[position])
   end
+
+def on_board?(num)
+  if num.between?(0,8) == true
+    return true
+  else
+    return false
+  end
+end
+  
+if (position_taken?(board, index)) == false && (on_board?(index)) == true
+  return true
+else
+  return false
+end
